72b526873d7fdc3112474e2f03b5be1d349242bc
AlexGoodD <alejandrochavarb@gmail.com>
2024-12-12 18:28:56 -0600

    Formateo de color

---
+++ TaskDone/Models/UIColor+Extensions.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+extension UIColor {
+    
+    func toHexString() -> String {
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        return String(format: "%02X%02X%02X", Int(r * 255), Int(g * 255), Int(b * 255))
+    }
+}
+
+extension Color {
+    
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+        let red = Double((rgbValue & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgbValue & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgbValue & 0x0000FF) / 255.0
+        self.init(red: red, green: green, blue: blue)
+    }
+
+    
+    func toUIColor() -> UIColor {
+        let components = self.cgColor?.components ?? [0, 0, 0, 0]
+        return UIColor(red: components[0], green: components[1], blue: components[2], alpha: components[3])
+    }
+}
