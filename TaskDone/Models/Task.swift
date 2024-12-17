e5ffd37596889a94fe6869f9f419d2503e3a6bea
AlexGoodD <alejandrochavarb@gmail.com>
2024-12-16 18:15:38 -0600

    2/2

--- TaskDone/Models/Task.swift
+++ TaskDone/Models/Task.swift
@@ -6,7 +6,6 @@ public class Task: NSManagedObject, Identifiable {
     @NSManaged public var id: UUID
     @NSManaged public var title: String
     @NSManaged public var isCompleted: Bool
-    @NSManaged public var isDelete: Bool
     @NSManaged public var creationDate: Date
     @NSManaged public var category: TaskCategory?
 }
